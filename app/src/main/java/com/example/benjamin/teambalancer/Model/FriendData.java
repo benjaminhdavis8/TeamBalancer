12239dee38ddbe406e8afc8d0b9737109323719a
benjamin <benjaminhdavis8@gmail.com>
2018-10-09 11:15:34 -0600

    adds xml to the verses fragment

--- app/src/main/java/com/example/benjamin/teambalancer/Model/FriendData.java
+++ app/src/main/java/com/example/benjamin/teambalancer/Model/FriendData.java
@@ -2,15 +2,17 @@ package com.example.benjamin.teambalancer.Model;
 
 
 public class FriendData {
+    private final Freind RealOne;
     public final String Username;
     public final String Rank;
     Boolean Selected;
     //Image personalImage
     //image rankImage
 
-    FriendData(Freind sorce) {
-        Username = sorce.Username;
-        Rank = sorce.Rank.toString();
+    FriendData(Freind source) {
+        RealOne = source;
+        Username = source.Username;
+        Rank = source.Rank.toString();
         Selected = false;
     }
 
